3b7e808d3bd4427443ff33f20d5a01f1b4bdbff1
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-06-02 02:03:14 -0400

    commit tree d07c1eac1382cacfde8fbcc89a48b122f16b1640

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+function superbowlWin(record){
+    const result = record.find(obj => obj.result === "W")
+     return (result ? result.year : sadReality[result])
+
+
+}
